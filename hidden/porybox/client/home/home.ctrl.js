7361dc3d596e9277ad745be73fce5dce34b06b88
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-03-16 19:30:01 -0400

    Add files via upload

---
+++ hidden/porybox/client/home/home.ctrl.js
@@ -0,0 +1,9 @@
+/**
+ * A small controller to explain the syntax we will be using
+ * @return {function} A controller that contains 2 test elements
+ */
+module.exports = function() {
+  if (this.selected) {
+    this.selected.selectedBox = null;
+  }
+};
